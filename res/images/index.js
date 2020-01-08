34d22ccce1149c234e75eb6cf5e044ab9695a898
Gwynever <lahrabzimohamed@gmail.com>
2020-01-08 17:57:52 +0100

    Removed unused files

--- res/images/index.js
+++ res/images/index.js
@@ -1,8 +1,5 @@
 const images = {
-  SpaceShipI: require('./spaceShip.png'),
   SpaceShipII: require('./img.png'),
-  SpaceShipIII: require('./resized.png'),
-  TestImg: require('./mMYoe.png'),
   spaceCraft: require('./spaceCraft.png'),
 };
 
--- src/scenes/space_2_0/Utils/index.js
+++
@@ -1,9 +0,0 @@
-export function Animator() {
-
-}
-
-export function random(min, max, integer = true) {
-	if (integer)
-		return Math.floor((Math.random() * max) + min);
-	return (Math.random() * max) + min;
-}
